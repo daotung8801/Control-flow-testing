325a5233b7a2ef010763cbfee41bf7257edc736f
daotung8801 <56082813+daotung8801@users.noreply.github.com>
2022-10-14 17:18:45 +0700

    Update test.c

--- test.c
+++ test.c
@@ -3,11 +3,11 @@
 float functionTest(int a, int b, int c) {
 	int sum = 0;
 	if (a + b == 4) {
-		return 0;
+	   return 0;
 	}
 	int d = 0;
 	if (a == b && b!= c) {
-		d = a + b;
+	   d = a + b;
 	}
 	sum = d + c;
 	return sum;
